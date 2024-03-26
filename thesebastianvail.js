9f0027fc7cfd77f36f6faf9c0ae688ec95cc78bb
tuhinahmed89 <163977639+tuhinahmed89@users.noreply.github.com>
2024-03-26 13:48:12 -0500

    Create thesebastianvail.js

---
+++ thesebastianvail.js
@@ -0,0 +1,237 @@
+{
+	"config": {
+		"live": "true", 
+		"features": {
+			"pageSpecific": "true",
+			"voiceSearch": "true",
+			"arrays": {
+				"restaurants": "true", 
+				"spas": "true", 
+				"healthClubs": "true", 
+				"rooms": "true",
+				"amenities": "true" 
+			}
+		}
+	},
+
+	"name": "The Sebastian - Vail",
+
+	"type": "Resort",
+
+	"homepage": "https://www.timberskauai.com",
+
+	"slogan": "Living the Spirit of Aloha",
+
+	"description": "Timbers Kaua’i is proud to be part of the Timbers Resorts portfolio. This collection of luxury properties is characterized by several unique elements that make our resorts the finest in the world. For more than two decades, Timbers Resorts has been crafting the settings for families and friends to come together and create lasting memories in some of the most special places on Earth.",
+
+	"petsAllowedTrueFalse": "false",
+
+	"totalRoomCount": "47",
+	"priceRange": "$$$",
+
+	"currenciesAccepted": ["USD"],
+
+	"paymentAccepted": ["Credit Card", "Debit Card"],
+
+	"availableLanguage": ["English"],
+
+	"checkInTime": "16:00",
+	"checkOutTime": "09:00",
+
+	"rating": {
+		"value": "4.8",
+		"count": "135"
+	},
+
+	"contact": {
+
+		"phone": "1-844-440-7572",
+
+		"email": "info@timberskauai.com"
+	},
+	
+	"location": {
+		"street": "3770 Ala’oli Way",
+		"city": "Lihue",
+		"state": "HI",
+		"zip": "96766",
+		"country": "USA",
+		"lat": "21.9583", 
+		"long": "-159.3451" 
+	},
+
+	"logoUrl": "https://www.timberskauai.com/wp-content/themes/kauai/assets/img/logo-kauai-white.png",
+
+	"copyrightUrl": "https://www.timbersresorts.com/privacy",
+
+	"facebookUrl": "https://www.facebook.com/timberskauai",
+	"twitterUrl": "null",
+	"instagramUrl": "https://www.instagram.com/timberskauai",
+	"linkedinUrl": "https://www.linkedin.com/company/timbers-kaua'i/about",
+
+	"amenitiesArr": [
+		
+		{
+			"name": "",
+			"url": ""
+		}
+	],
+
+	"imageArr": [
+		{
+			"caption": "own with us",
+			"url": "https://www.timberskauai.com/wp-content/uploads/2023/02/TKAU_maliula_2102_liv_rm_view_95214_lo_res_RGB-960x960.jpg"
+		},
+		{
+			"caption": "stay with us",
+			"url": "https://www.timberskauai.com/wp-content/uploads/2023/02/TKAU_kaiholo_3109_master_bed_93994_lo_res_RGB-e1682469385442-718x730.jpg"
+		},
+		{
+			"caption": "maliula residences",
+			"url": "https://www.timberskauai.com/wp-content/uploads/2023/02/TKAU_maliula_2302_master_bed_94999_lo_res_RGB-1775x800.jpg"
+		},
+		{
+			"caption": "kaiholo residences",
+			"url": "https://www.timberskauai.com/wp-content/uploads/2023/02/TKAU_Kaiholo_3106_Livingroom2018_07_19_looking_out_v22018_07_19_lo_res_RGB-1775x800.jpg"
+		},
+		{
+			"caption": "Laola Residences",
+			"url": "https://www.timberskauai.com/wp-content/uploads/2023/02/KAU_laola_1004_lr_4660_lo_res_RGB-1775x800.jpg"
+		},
+		{
+			"caption": "Laola Nani Residences",
+			"url": "https://www.timberskauai.com/wp-content/uploads/2023/06/Laola-Nani-Residence-Style-D-002-A-1500x800.jpg"
+		},
+		{
+			"caption": "Book Early & Save",
+			"url": "https://www.timberskauai.com/wp-content/uploads/2023/03/DSC_1311-570x372.jpg"
+		},
+		{
+			"caption": "4th Night Free",
+			"url": "https://www.timberskauai.com/wp-content/uploads/2023/02/TKAU_maliula_2302_dbl_bedrm_95084_lo_res_RGB-570x372.jpg"
+		},
+		{
+			"caption": "Experience Hōkūala",
+			"url": "https://www.timberskauai.com/wp-content/uploads/2023/02/KAU_Aerial_0021_lo_res_RGB-570x372.jpg"
+		},
+		{
+			"caption": "The Ocean Course at Hōkūala",
+			"url": "https://www.timberskauai.com/wp-content/uploads/2023/02/KAU_Golf_Dyer_3154_2015_10_05_lo_res_RGB-747x460.jpg"
+		}
+		
+	],
+
+	"roomsArr": [
+	
+		{
+			"type": "",
+			"name": "",
+			"url": "",
+			"bookUrl": "",
+			"tourUrl": "",
+			"petsAllowedTrueOrFalse": "",
+			"description": "",
+			"maxOccupancy": "",
+			"checkInTime": "",
+			"checkOutTime": "",
+			"bedCount": "",
+			"bedType": "",
+			"smokingBoolean": "",
+			"bathroomCount": "",
+			"roomCount": "",
+			
+			"images": [
+				{
+					"caption": "",
+					"url": ""
+				}
+			],
+			"roomAmenitiesArr": [
+
+				{
+					"name": "",
+					"url": ""
+				}
+			]
+		}
+	],
+
+	"restaurants": [
+		
+		{
+			
+			"name": "",
+			"url": "",
+			"description": "",
+
+			"menu": [
+				{
+					"name": "Breakfast",
+					"url": "https://www.globalambassadorhotel.com/pdf/LeAmeBreakfast_0224.pdf"
+				}
+				],
+
+			"priceRange": "$$$",
+
+			"contact": {
+
+				"phone": "", 
+				"email": ""
+				
+			},
+			
+			"location": {
+				"street": "",
+				"city": "",
+				"state": "", 
+				"zip": "",
+				"country": ""
+			},
+			
+			
+			"rating": {
+				"value": "",
+				"count": ""
+			},
+
+			
+			"openingHours": [],
+			"servesCuisine": []
+			
+		}
+	],
+
+	"spas": [
+		
+		{
+			"name": "",
+			"url": "",
+			"description": ""
+		}
+		
+	],
+
+	"healthClubs": [
+		
+		{
+			"name": "",
+			"url": "",
+			"description": ""
+		}
+		
+	],
+
+	"reviews": [
+		{
+			"", 
+			"value": "",
+			"datePublished": "",
+			"reviewed": {
+
+				"type": "VacationRental", 
+				"name": ""
+			}
+		},
+	]
+	
+}
