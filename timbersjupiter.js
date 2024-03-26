9eff7f4b0fa3abcf7ca91a67be2327854329d14b
tuhinahmed89 <163977639+tuhinahmed89@users.noreply.github.com>
2024-03-26 17:55:55 -0500

    Create timbersjupiter.js

---
+++ timbersjupiter.js
@@ -0,0 +1,208 @@
+{
+	"config": {
+		"live": "true", 
+		"features": {
+			"pageSpecific": "true",
+			"voiceSearch": "true",
+			"arrays": {
+				"restaurants": "true", 
+				"spas": "true", 
+				"healthClubs": "true", 
+				"rooms": "true",
+				"amenities": "true" 
+			}
+		}
+	},
+
+	"name": "Casali di Casole",
+
+	"type": "Resort",
+
+	"homepage": "https://www.casalidicasole.com",
+
+	"slogan": "Always so much more than a place, Tuscany is first a way of life",
+
+	"description": "In the rolling hills of Tuscany — not far from the mythic destinations of Florence, Siena and San Gimignano — is one of the largest private landholdings in all of Italy. Dotting the 4,200-acre historic estate are meticulously restored and exquisitely appointed villas and farmhouses. This treasured place is Casali di Casole, a Timbers Resorts private residence club and the best luxury vacation rental in Italy.",
+
+	"petsAllowedTrueFalse": "true",
+
+	"totalRoomCount": "",
+	"priceRange": "$$$",
+
+	"currenciesAccepted": ["USD"],
+
+	"paymentAccepted": ["Cash", "Credit Card", "Debit Card"],
+
+	"availableLanguage": ["English", "French", "Italian"],
+
+	"checkInTime": "15:00",
+	"checkOutTime": "07:00",
+
+	"rating": {
+		"value": "4.4",
+		"count": "19"
+	},
+
+	"contact": {
+
+		"phone": "1-888-548-9429",
+
+		"email": "reservations@casalidicasole.com"
+	},
+	
+	"location": {
+		"street": "Località Ponti, 2",
+		"city": "Siena",
+		"state": "Tuscany",
+		"zip": "53031",
+		"country": "Italy",
+		"lat": "43.3072", 
+		"long": "-11.1131" 
+	},
+
+	"logoUrl": "https://www.casalidicasole.com/wp-content/themes/casali/assets/img/logo-casalidicasole.png",
+
+	"copyrightUrl": "https://www.casalidicasole.com/privacy-policy",
+
+	"facebookUrl": "https://www.facebook.com/casalidicasole",
+	"twitterUrl": "https://twitter.com/CasoleDi",
+	"instagramUrl": "https://www.instagram.com/casalidicasole",
+	"linkedinUrl": "https://www.linkedin.com/company/casali-di-casole/about",
+
+	"amenitiesArr": [
+		
+		{
+			"name": "",
+			"url": ""
+		}
+	],
+
+	"imageArr": [
+		{
+			"caption": "stay with us",
+			"url": "https://www.casalidicasole.com/wp-content/uploads/2023/01/CDC_SanGiovanni_Bedroom_lo_res_RGB-11-1-950x475.jpg"
+		},
+		{
+			"caption": "Activities in Tuscany",
+			"url": "https://www.casalidicasole.com/wp-content/uploads/2023/01/CDC_1067_I2A0348-1-747x460.jpg"
+		},
+		{
+			"caption": "A Historic Estate",
+			"url": "https://www.casalidicasole.com/wp-content/uploads/2023/02/CDC-EstateSelects-LoResRGB-7-1440x800.jpeg"
+		}
+	],
+
+	"roomsArr": [
+	
+		{
+			"type": "",
+			"name": "",
+			"url": "",
+			"bookUrl": "",
+			"tourUrl": "",
+			"petsAllowedTrueOrFalse": "",
+			"description": "",
+			"maxOccupancy": "",
+			"checkInTime": "",
+			"checkOutTime": "",
+			"bedCount": "",
+			"bedType": "",
+			"smokingBoolean": "",
+			"bathroomCount": "",
+			"roomCount": "",
+			
+			"images": [
+				{
+					"caption": "",
+					"url": ""
+				}
+			],
+			"roomAmenitiesArr": [
+
+				{
+					"name": "",
+					"url": ""
+				}
+			]
+		}
+	],
+
+	"restaurants": [
+		
+		{
+			
+			"name": "",
+			"url": "",
+			"description": "",
+
+			"menu": [
+				{
+					"name": "Breakfast",
+					"url": "https://www.globalambassadorhotel.com/pdf/LeAmeBreakfast_0224.pdf"
+				}
+				],
+
+			"priceRange": "$$$",
+
+			"contact": {
+
+				"phone": "", 
+				"email": ""
+				
+			},
+			
+			"location": {
+				"street": "",
+				"city": "",
+				"state": "", 
+				"zip": "",
+				"country": ""
+			},
+			
+			
+			"rating": {
+				"value": "",
+				"count": ""
+			},
+
+			
+			"openingHours": [],
+			"servesCuisine": []
+			
+		}
+	],
+
+	"spas": [
+		
+		{
+			"name": "",
+			"url": "",
+			"description": ""
+		}
+		
+	],
+
+	"healthClubs": [
+		
+		{
+			"name": "",
+			"url": "",
+			"description": ""
+		}
+		
+	],
+
+	"reviews": [
+		{
+			"", 
+			"value": "",
+			"datePublished": "",
+			"reviewed": {
+
+				"type": "VacationRental", 
+				"name": ""
+			}
+		},
+	]
+	
+}
