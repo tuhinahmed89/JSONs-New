d65d60bc07eadaed5937e535380af2ec17f2fe7e
tuhinahmed89 <163977639+tuhinahmed89@users.noreply.github.com>
2024-03-27 13:58:52 -0500

    Update 5th&55thresidenceclub.js

--- 5th&55thresidenceclub.js
+++ 5th&55thresidenceclub.js
@@ -1,1 +1,213 @@
+{
+	"config": {
+		"live": "true", 
+		"features": {
+			"pageSpecific": "true",
+			"voiceSearch": "true",
+			"arrays": {
+				"restaurants": "true", 
+				"spas": "true", 
+				"healthClubs": "true", 
+				"rooms": "true",
+				"amenities": "true" 
+			}
+		}
+	},
+
+	"name": "5th & 55th Residence Club",
+
+	"type": "Hotel",
+
+	"homepage": "https://www.andiamocollection.com/stay-with-us/5th-55th-residence-club",
+
+	"slogan": "The Jewel of the Atlantic Coast",
+
+	"description": "Named for the Roman king of the gods, the town of Jupiter, along with the neighboring Palm Beaches, entices the vacationing family with an endless variety of outdoor water sports as well as a vibrant dining, shopping and cultural scene – one reason it is considered to be the jewel of the Atlantic coast. In the center of this captivating setting lies Timbers Jupiter, a private residence club nestled within the acclaimed 285-acre Trump National Golf Club Jupiter and only minutes inland from the glistening shores of Juno Beach. Owners at Timbers Jupiter will enjoy an iconic and leisurely Florida lifestyle highlighted by spacious and very private villas overlooking the course and its many water features.",
+
+	"petsAllowedTrueFalse": "false",
+
+	"totalRoomCount": "50",
+	"priceRange": "$$$",
+
+	"currenciesAccepted": ["USD"],
+
+	"paymentAccepted": ["Cash", "Credit Card", "Debit Card"],
+
+	"availableLanguage": ["English"],
+
+	"checkInTime": "16:00",
+	"checkOutTime": "12:00",
+
+	"rating": {
+		"value": "4",
+		"count": "4"
+	},
+
+	"contact": {
+
+		"phone": "1-561-295-7609",
+
+		"email": "info@timbersjupiter.com"
+	},
+	
+	"location": {
+		"street": "108 Night Hawk Drive",
+		"city": "Jupiter",
+		"state": "FL",
+		"zip": "33477",
+		"country": "US",
+		"lat": "26.8837",
+		"long": "-80.0933" 
+	},
+
+	"logoUrl": "https://www.timbersjupiter.com/wp-content/themes/jupiter/assets/img/logo-jupiter.png",
+
+	"copyrightUrl": "https://www.timbersjupiter.com/privacy-policy",
+
+	"facebookUrl": "https://www.facebook.com/TimbersJupiter",
+	"twitterUrl": "https://twitter.com/TimbersJupiter",
+	"instagramUrl": "https://www.instagram.com/timbersjupiter",
+	"linkedinUrl": "https://www.linkedin.com/company/timbers-resorts",
+
+	"amenitiesArr": [
+		
+		{
+			"name": "",
+			"url": ""
+		}
+	],
+
+	"imageArr": [
+		{
+			"caption": "own with us",
+			"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/Timbers-Jupiter-57-960x960.jpg"
+		},
+		{
+			"caption": "stay with us",
+			"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/Timbers-Jupiter-19-960x960.jpg"
+		},
+		{
+			"caption": "Golf",
+			"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Experience_Golf_Cart-1050x704.jpeg"
+		},
+		{
+			"caption": "Golf",
+			"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Golf__128_lo_res_RGB-1-747x460.jpg"
+		}
+	],
+
+	"roomsArr": [
+	
+		{
+			"type": "",
+			"name": "",
+			"url": "",
+			"bookUrl": "",
+			"tourUrl": "",
+			"petsAllowedTrueOrFalse": "",
+			"description": "",
+			"maxOccupancy": "",
+			"checkInTime": "",
+			"checkOutTime": "",
+			"bedCount": "",
+			"bedType": "",
+			"smokingBoolean": "",
+			"bathroomCount": "",
+			"roomCount": "",
+			
+			"images": [
+				{
+					"caption": "",
+					"url": ""
+				}
+			],
+			"roomAmenitiesArr": [
+
+				{
+					"name": "",
+					"url": ""
+				}
+			]
+		}
+	],
+
+	"restaurants": [
+		
+		{
+			
+			"name": "",
+			"url": "",
+			"description": "",
+
+			"menu": [
+				{
+					"name": "Breakfast",
+					"url": "https://www.globalambassadorhotel.com/pdf/LeAmeBreakfast_0224.pdf"
+				}
+				],
+
+			"priceRange": "$$$",
+
+			"contact": {
+
+				"phone": "", 
+				"email": ""
+				
+			},
+			
+			"location": {
+				"street": "",
+				"city": "",
+				"state": "", 
+				"zip": "",
+				"country": ""
+			},
+			
+			
+			"rating": {
+				"value": "",
+				"count": ""
+			},
+
+			
+			"openingHours": [],
+			"servesCuisine": []
+			
+		}
+	],
+
+	"spas": [
+		
+		{
+			"name": "",
+			"url": "",
+			"description": ""
+		}
+		
+	],
+
+	"healthClubs": [
+		
+		{
+			"name": "",
+			"url": "",
+			"description": ""
+		}
+		
+	],
+
+	"reviews": [
+		{
+			"", 
+			"value": "",
+			"datePublished": "",
+			"reviewed": {
+
+				"type": "VacationRental", 
+				"name": ""
+			}
+		},
+	]
+	
+}
 
