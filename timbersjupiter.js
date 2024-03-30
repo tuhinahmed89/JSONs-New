98d6c9f67260dfc3e5a3b958ec7a6528d50e5d71
tuhinahmed89 <163977639+tuhinahmed89@users.noreply.github.com>
2024-03-30 14:15:36 -0500

    Update timbersjupiter.js

--- timbersjupiter.js
+++ timbersjupiter.js
@@ -99,33 +99,429 @@
 	"roomsArr": [
 	
 		{
-			"type": "",
-			"name": "",
-			"url": "",
-			"bookUrl": "",
+			"type": "Room",
+			"name": "Two Bedroom Residence",
+			"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa",
+			"bookUrl": "https://be.synxis.com/?chain=22824&currency=USD&hotel=31514&level=hotel&locale=en-US&start=availresults&arrive=03%2F14%2F2024&depart=03%2F18%2F2024&adult=2&child=0&_gl=1*130iyau*_ga*MTIwMjA5NjQ4LjE3MTEzOTc5OTY.*_ga_RD34KVPZET*MTcxMTgxOTU5MC4xMS4xLjE3MTE4MjAzNTMuNjAuMC4w&_ga=2.86616283.296555178.1711819590-120209648.1711397996&_gac=1.217836132.1711550975.CjwKCAjwh4-wBhB3EiwAeJsppOmVVjNoPXDJ0zvd9IRpcOQhVQ6OGWwjR6712eDg06ob-z8dOfKYHRoCpCYQAvD_BwE",
 			"tourUrl": "",
-			"petsAllowedTrueOrFalse": "",
-			"description": "",
-			"maxOccupancy": "",
-			"checkInTime": "",
-			"checkOutTime": "",
-			"bedCount": "",
-			"bedType": "",
+			"petsAllowedTrueOrFalse": "false",
+			"description": "Our two-bedroom luxury homes in Jupiter, Florida offer an ideal resort lifestyle, where every convenience and residential amenity is at hand, including daily housekeeping and villa services. Very private, very exclusive, each of these 21 Mediterranean-style freestanding homes overlooks a world-renowned golf course and has just over 1,900 square feet of exquisitely appointed living space. They are artfully furnished and bring the outdoors inside with expansive, screened-in exterior lanais that include a built-in barbecue and heated, jetted plunge pool.",
+			"maxOccupancy": "6",
+			"checkInTime": "14:00",
+			"checkOutTime": "12:00",
+			"bedCount": "2",
+			"bedType": "2 King Beds",
 			"smokingBoolean": "",
-			"bathroomCount": "",
-			"roomCount": "",
+			"bathroomCount": "2.5",
+			"roomCount": "2",
 			
 			"images": [
 				{
-					"caption": "",
-					"url": ""
+					"caption": "Residence Interiors masterbed 1",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors__masterbed1_39959_lo_res_RGB-1440x800.jpg"
+				},
+				{
+					"caption": "Residence Interiors",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors_Own_lo_res_RGB.jpg"
+				},
+				{
+					"caption": "Residence Interiors Living Room",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors__lr_40057_lo_res_RGB-1440x800.jpg"
+				},
+				{
+					"caption": "Residence Interiors Bathroom",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors__bath_40010_lo_res_RGB-1440x800.jpg"
+				},
+				{
+					"caption": "Residence Interiors Kitchen",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors__kitchen_40168_lo_res_RGB-1440x800.jpg"
+				},
+				{
+					"caption": "Residence Interiors masterbed 2",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors__master2_39988_lo_res_RGB-1440x800.jpg"
+				}
+				
+			],
+			"roomAmenitiesArr": [
+
+				{
+					"name": "Closets in room",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Coffee/Tea maker",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Fireplace",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Non-smoking",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Oven",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Private bathroom",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Quiet Room",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Bathroom amenities",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Smoke detectors",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Dish-cleaning supplies",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Hairdryer",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Washer/dryer",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Sitting area",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Barbeque grills",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Desk",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Double vanity",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Family/oversized room",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Ironing board",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Lighted makeup mirror",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Marble bathroom",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Private pool",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Self-controlled heating/cooling system",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Separate tub and shower",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "AM/FM radio",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Complimentary high speed internet in room",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "High Definition (HD) Flat Panel Television - 32 inches or greater",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Microwave",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Stove",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Alarm clock",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Down/feather pillows",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Hypoallergenic bed",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Kitchen supplies",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Balcony/Lanai/Terrace",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Ceiling fan",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "High ceilings",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Fire alarm",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Iron",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Kitchen",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Bathrobe",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				},
+				{
+					"name": "Dishwasher",
+					"url": "https://www.timbersjupiter.com/stay/two-bedroom-villa"
+				}
+			]
+		},
+		{
+			"type": "Room",
+			"name": "Four Bedroom Residence",
+			"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa",
+			"bookUrl": "https://be.synxis.com/?_ga=2.125461321.296555178.1711819590-120209648.1711397996&_gac=1.19418826.1711550975.CjwKCAjwh4-wBhB3EiwAeJsppOmVVjNoPXDJ0zvd9IRpcOQhVQ6OGWwjR6712eDg06ob-z8dOfKYHRoCpCYQAvD_BwE&_gl=1*wrb9vm*_ga*MTIwMjA5NjQ4LjE3MTEzOTc5OTY.*_ga_RD34KVPZET*MTcxMTgxOTU5MC4xMS4xLjE3MTE4MjI2NTMuOC4wLjA.&adult=2&arrive=2024-03-30&chain=22824&child=0&currency=USD&depart=2024-03-31&hotel=31514&level=hotel&locale=en-US&rooms=1&start=availresults",
+			"tourUrl": "",
+			"petsAllowedTrueOrFalse": "false",
+			"description": "The exceptional four-bedroom homes of Timbers Jupiter are the ultimate Florida retreat. Beautifully located in a luxury golf community, with every convenience and residential amenity right at your doorstep, they offer over 2,900 square feet of luxurious living space extended over two stories — an ideal choice if you need more room for family and friends.",
+			"maxOccupancy": "8",
+			"checkInTime": "14:00",
+			"checkOutTime": "12:00",
+			"bedCount": "4",
+			"bedType": "3 King & 1 Twin Beds",
+			"smokingBoolean": "",
+			"bathroomCount": "4.5",
+			"roomCount": "4",
+			
+			"images": [
+				{
+					"caption": "Residence Interiors masterbed 2",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors__master2_39988_lo_res_RGB-1440x800.jpg"
+				},
+				{
+					"caption": "Residence Interiors masterbed 1",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors__masterbed1_39959_lo_res_RGB-1440x800.jpg"
+				},
+				{
+					"caption": "Residence Interiors Kitchen",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors__kitchen_40168_lo_res_RGB-1440x800.jpg"
+				},
+				{
+					"caption": "Residence Interiors Lanai 2",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors__lanai2_40192_lo_res_RGB-1440x800.jpg"
+				},
+				{
+					"caption": "Residence Interiors Lanai 1",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors__lanai1_40035_lo_res_RGB-1440x800.jpg"
+				},
+				{
+					"caption": "Residence Interiors Dining",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors__dining_40104_lo_res_RGB-1440x800.jpg"
+				},
+				{
+					"caption": "Residence Interiors",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors_Own_lo_res_RGB.jpg"
+				},
+				{
+					"caption": "Residence Interiors Living Room",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors__lr_40057_lo_res_RGB-1440x800.jpg"
+				},
+				{
+					"caption": "Residence Interiors Bathroom",
+					"url": "https://www.timbersjupiter.com/wp-content/uploads/2023/03/JUP_Residence_Interiors__bath_40010_lo_res_RGB-1440x800.jpg"
 				}
 			],
 			"roomAmenitiesArr": [
 
 				{
-					"name": "",
-					"url": ""
+					"name": "Closets in room",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Coffee/Tea maker",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Fireplace",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Non-smoking",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Oven",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Private bathroom",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Quiet Room",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Bathroom amenities",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Smoke detectors",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Dish-cleaning supplies",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Hairdryer",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Washer/dryer",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Sitting area",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Barbeque grills",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Desk",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Double vanity",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Family/oversized room",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Ironing board",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Lighted makeup mirror",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Marble bathroom",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Private pool",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Self-controlled heating/cooling system",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Separate tub and shower",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "AM/FM radio",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Complimentary high speed internet in room",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "High Definition (HD) Flat Panel Television - 32 inches or greater",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Microwave",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Stove",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Alarm clock",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Down/feather pillows",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Hypoallergenic bed",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Kitchen supplies",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Balcony/Lanai/Terrace",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Ceiling fan",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "High ceilings",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Fire alarm",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Iron",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Kitchen",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Bathrobe",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
+				},
+				{
+					"name": "Dishwasher",
+					"url": "https://www.timbersjupiter.com/stay/four-bedroom-villa"
 				}
 			]
 		}
@@ -141,8 +537,8 @@
 
 			"menu": [
 				{
-					"name": "Breakfast",
-					"url": "https://www.globalambassadorhotel.com/pdf/LeAmeBreakfast_0224.pdf"
+					"name": "",
+					"url": ""
 				}
 				],
 
@@ -179,9 +575,9 @@
 	"spas": [
 		
 		{
-			"name": "",
-			"url": "",
-			"description": ""
+			"name": "The Spa At Trump National Golf Club, Jupiter",
+			"url": "https://www.trumpnationaljupiter.com/spa",
+			"description": "Luxury is the perfect course for relaxation at the most extraordinary private spa in South Florida. Experience the exclusive and serene environment at the members-only spa offering unparalleled five-star service.The luxuriously appointed space includes ten treatment rooms with one couples' suite, a full-service hair and nail salon, separate male and female Jacuzzis, steam rooms, saunas and relaxation lounges curated with upgraded amenities."
 		}
 		
 	],
@@ -198,15 +594,26 @@
 
 	"reviews": [
 		{
-			"", 
-			"value": "",
-			"datePublished": "",
+			"author": "Mariano Balderas"			, 
+			"value": "5",
+			"datePublished": "2023-03-30",
 			"reviewed": {
 
 				"type": "VacationRental", 
 				"name": ""
 			}
 		},
+		{
+			"author": "Shaun Guarini", 
+			"value": "5", 
+			"datePublished": "2023-03-30",
+			"reviewed": {
+
+				
+				"type": "VacationRental",
+				"name": ""
+			}
+		}
 	]
 	
 }
